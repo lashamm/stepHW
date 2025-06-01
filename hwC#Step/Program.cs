db13bf3f03078ad19ad3af82338e15fbbd47bb0c
lasha <maisuradze.llasha@gmail.com>
2025-06-01 09:44:53 +0400

    first hw done

--- hwC#Step/Program.cs
+++ hwC#Step/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿string name;
+string lastName;
+int age;
+string favoriteColor;
+string favoriteFood;
+
+Console.WriteLine("enter your name");
+name = Console.ReadLine();
+
+Console.WriteLine("enter your LastName");
+lastName = Console.ReadLine();
+
+Console.WriteLine("enter your age");
+age = int.Parse(Console.ReadLine());
+
+Console.WriteLine("enter your favorite color");
+favoriteColor = Console.ReadLine();
+
+Console.WriteLine("enter your favorite food");
+favoriteFood = Console.ReadLine();
+
+Console.WriteLine($"your name is {name} your last name is {lastName} you are {age} years old your favourite color is {favoriteColor} and your favorite food is {favoriteFood}");
