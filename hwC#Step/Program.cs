9acd64986edb7437e987d00cc2021853ed212e27
lasha <maisuradze.llasha@gmail.com>
2025-06-01 20:43:11 +0400

    homework done

--- hwC#Step/Program.cs
+++ hwC#Step/Program.cs
@@ -1,39 +1,55 @@
 ﻿//                                   HW 1
 
-//string name;
-//string lastName;
-//int age;
-//string favoriteColor;
-//string favoriteFood;
+string name;
+string lastName;
+int age;
+string favoriteColor;
+string favoriteFood;
 
-//Console.WriteLine("enter your name");
-//name = Console.ReadLine();
+Console.WriteLine("enter your name");
+name = Console.ReadLine();
 
-//Console.WriteLine("enter your LastName");
-//lastName = Console.ReadLine();
+Console.WriteLine("enter your LastName");
+lastName = Console.ReadLine();
 
-//Console.WriteLine("enter your age");
-//age = int.Parse(Console.ReadLine());
+Console.WriteLine("enter your age");
+age = int.Parse(Console.ReadLine());
 
-//Console.WriteLine("enter your favorite color");
-//favoriteColor = Console.ReadLine();
+Console.WriteLine("enter your favorite color");
+favoriteColor = Console.ReadLine();
 
-//Console.WriteLine("enter your favorite food");
-//favoriteFood = Console.ReadLine();
+Console.WriteLine("enter your favorite food");
+favoriteFood = Console.ReadLine();
 
-//Console.WriteLine($"your name is {name}" +
-//    $" your last name is {lastName}" +
-//    $" you are {age}" +
-//    $" years old your favourite color is {favoriteColor}" +
-//    $" and your favorite food is {favoriteFood}");
+Console.WriteLine($"your name is {name}" +
+    $" your last name is {lastName}" +
+    $" you are {age}" +
+    $" years old your favourite color is {favoriteColor}" +
+    $" and your favorite food is {favoriteFood}");
+
+
+
+
 
 
 
 //                                  HW 2 
 
 
-double balance = 1000.0;
-double depositAmount;
-double withdrawAmount;
+//double balance;
+//double depositAmount;
+//double withdrawAmount;
 
-Console.WriteLine($"You have {balance} GEL in your bank account");
+//Console.WriteLine("Enter your balance:");
+//balance = double.Parse(Console.ReadLine());
+
+//Console.WriteLine("Enter the amount you want to deposit:");
+//depositAmount = double.Parse(Console.ReadLine());
+
+//Console.WriteLine("Enter the amount you want to withdraw:");
+//withdrawAmount = double.Parse(Console.ReadLine());
+
+//Console.Clear();
+
+//Console.WriteLine($"Your balance is: {balance + depositAmount - withdrawAmount}");
+
