eade4552f9505decc146a86e727df68112e0dcb4
lasha <maisuradze.llasha@gmail.com>
2025-06-01 09:55:51 +0400

    will finish later

--- hwC#Step/Program.cs
+++ hwC#Step/Program.cs
@@ -1,22 +1,39 @@
-﻿string name;
-string lastName;
-int age;
-string favoriteColor;
-string favoriteFood;
+﻿//                                   HW 1
 
-Console.WriteLine("enter your name");
-name = Console.ReadLine();
+//string name;
+//string lastName;
+//int age;
+//string favoriteColor;
+//string favoriteFood;
 
-Console.WriteLine("enter your LastName");
-lastName = Console.ReadLine();
+//Console.WriteLine("enter your name");
+//name = Console.ReadLine();
 
-Console.WriteLine("enter your age");
-age = int.Parse(Console.ReadLine());
+//Console.WriteLine("enter your LastName");
+//lastName = Console.ReadLine();
 
-Console.WriteLine("enter your favorite color");
-favoriteColor = Console.ReadLine();
+//Console.WriteLine("enter your age");
+//age = int.Parse(Console.ReadLine());
 
-Console.WriteLine("enter your favorite food");
-favoriteFood = Console.ReadLine();
+//Console.WriteLine("enter your favorite color");
+//favoriteColor = Console.ReadLine();
 
-Console.WriteLine($"your name is {name} your last name is {lastName} you are {age} years old your favourite color is {favoriteColor} and your favorite food is {favoriteFood}");
+//Console.WriteLine("enter your favorite food");
+//favoriteFood = Console.ReadLine();
+
+//Console.WriteLine($"your name is {name}" +
+//    $" your last name is {lastName}" +
+//    $" you are {age}" +
+//    $" years old your favourite color is {favoriteColor}" +
+//    $" and your favorite food is {favoriteFood}");
+
+
+
+//                                  HW 2 
+
+
+double balance = 1000.0;
+double depositAmount;
+double withdrawAmount;
+
+Console.WriteLine($"You have {balance} GEL in your bank account");
