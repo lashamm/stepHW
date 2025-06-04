04b05ce4ffe802a150c1a4904cca4605c6eb65ea
lasha <maisuradze.llasha@gmail.com>
2025-06-04 14:08:08 +0400

    2 hw done

--- hwC#Step/Program.cs
+++ hwC#Step/Program.cs
@@ -1,34 +1,134 @@
 ﻿
 
 
+//                                             2nd les HW 1
+
+
+//int year;
+//Console.WriteLine("Enter a random year:");
+//year = int.Parse(Console.ReadLine());
+
+//if(year % 4 == 0)
+//{
+//    Console.WriteLine("leap year");
+//}
+//else
+//{
+//    Console.WriteLine("not a leap year");
+//}
+
+
+
+//                                            2nd les HW 2
+
+//int firstQuestion;
+//int secondQuestion;
+//int thirdQuestion;
+//int fourthQuestion;
+//int fifthQuestion;
+//int finalResult = 0;
+
+//Console.WriteLine("2+2");
+//firstQuestion = int.Parse(Console.ReadLine());
+//Console.WriteLine("3+1");
+//secondQuestion = int.Parse(Console.ReadLine());
+//Console.WriteLine("5-1");
+//thirdQuestion = int.Parse(Console.ReadLine());
+//Console.WriteLine("6/2");
+//fourthQuestion = int.Parse(Console.ReadLine());
+//Console.WriteLine("2*3");
+//fifthQuestion = int.Parse(Console.ReadLine());
+
+//if (firstQuestion == 4) { 
+//finalResult += 20;
+//}
+//else{
+//    finalResult -= 20;
+//}
+//if (secondQuestion == 4){
+//    finalResult += 20;
+//}
+//else{
+//    finalResult -= 0;
+//}
+//if (thirdQuestion == 4){
+//    finalResult += 20;
+//}
+//else{
+//    finalResult -= 0;
+//}
+//if (fourthQuestion == 3){
+//    finalResult += 20;
+//}
+//else{
+//    finalResult -= 0;
+//}
+//if (fifthQuestion == 6){
+//    finalResult += 20;
+//}
+//else{
+//    finalResult -= 0;
+//}
+
+//if (finalResult >= 60){
+//    Console.WriteLine($"You passed the test your score is {finalResult}");
+//}
+//else{
+//    Console.WriteLine($"You failed the test, your score is {finalResult} you need atleast 60");
+//}
+
+
+
+//                                            2nd les HW 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //                             1st les HW 1
 
-string name;
-string lastName;
-int age;
-string favoriteColor;
-string favoriteFood;
+//string name;
+//string lastName;
+//int age;
+//string favoriteColor;
+//string favoriteFood;
 
-Console.WriteLine("enter your name");
-name = Console.ReadLine();
+//Console.WriteLine("enter your name");
+//name = Console.ReadLine();
 
-Console.WriteLine("enter your LastName");
-lastName = Console.ReadLine();
+//Console.WriteLine("enter your LastName");
+//lastName = Console.ReadLine();
 
-Console.WriteLine("enter your age");
-age = int.Parse(Console.ReadLine());
+//Console.WriteLine("enter your age");
+//age = int.Parse(Console.ReadLine());
 
-Console.WriteLine("enter your favorite color");
-favoriteColor = Console.ReadLine();
+//Console.WriteLine("enter your favorite color");
+//favoriteColor = Console.ReadLine();
 
-Console.WriteLine("enter your favorite food");
-favoriteFood = Console.ReadLine();
+//Console.WriteLine("enter your favorite food");
+//favoriteFood = Console.ReadLine();
 
-Console.WriteLine($"your name is {name}" +
-    $" your last name is {lastName}" +
-    $" you are {age}" +
-    $" years old your favourite color is {favoriteColor}" +
-    $" and your favorite food is {favoriteFood}");
+//Console.WriteLine($"your name is {name}" +
+//    $" your last name is {lastName}" +
+//    $" you are {age}" +
+//    $" years old your favourite color is {favoriteColor}" +
+//    $" and your favorite food is {favoriteFood}");
 
 
 
