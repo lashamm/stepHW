d535487df0488475addc23e45de73bcedb75235a
lasha <maisuradze.llasha@gmail.com>
2025-06-11 20:15:59 +0400

    second hw done

--- hwC#Step/Program.cs
+++ hwC#Step/Program.cs
@@ -1,23 +1,38 @@
 ﻿//                                   4th les HW 1
 
-int[] nums1 = {1,2,3,4};
-int[] nums2 = {5,6,7,8};
-int[] resultArray = new int[nums1.Length + nums2.Length];
+//int[] nums1 = {1,2,3,4};
+//int[] nums2 = {5,6,7,8};
+//int[] resultArray = new int[nums1.Length + nums2.Length];
 
-for(int i = 0; i < nums1.Length; i++)
-{
-    resultArray[i] = nums1[i];
-}
-for(int i = 0; i < nums2.Length; i++)
-{
-    resultArray[nums1.Length + i] = nums2[i];
-}
-for(int i = 0; i < resultArray.Length; i++)
-{
-    Console.WriteLine(resultArray[i]);
-}
+//for(int i = 0; i < nums1.Length; i++)
+//{
+//    resultArray[i] = nums1[i];
+//}
+//for(int i = 0; i < nums2.Length; i++)
+//{
+//    resultArray[nums1.Length + i] = nums2[i];
+//}
+//for(int i = 0; i < resultArray.Length; i++)
+//{
+//    Console.WriteLine(resultArray[i]);
+//}
 
 
+//                                   4th les HW 2
+
+//int targetSum = 10;
+//int[] nums = { 2, 6, 7, 12, 3, 7, 11, 9, 5, 1, 4, 8 };
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    for(int j = i + 1; j < nums.Length; j++)
+//    {
+//        if (nums[i] + nums[j] == targetSum)
+//        {
+//            Console.WriteLine($"Pair found: {nums[i]} + {nums[j]} = {targetSum}");
+//        }
+//    }
+//}
 
 
 
