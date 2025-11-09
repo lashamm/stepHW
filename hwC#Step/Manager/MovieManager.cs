354b3912690cc1ac2c02622acd2d3b34e70ed7cc
lasha <maisuradze.llasha@gmail.com>
2025-11-09 13:52:19 +0400

    this hw done

---
+++ hwC#Step/Manager/MovieManager.cs
@@ -0,0 +1,70 @@
+﻿using ConsoleApp_418_FinalProject.Models;
+
+namespace ConsoleApp_418_FinalProject.Manager
+{
+    internal class MovieManager : Icrud
+    {
+        private List<Movie> _movies;
+
+        public MovieManager()
+        {
+            _movies = new List<Movie>();
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            _movies.Add(movie);
+        }
+
+        public void ShowAll()
+        {
+            _movies.ForEach(Console.WriteLine);
+        }
+
+        public bool GetMovie(string title, out Movie? movie)
+        {
+            Func<Movie, bool> func = (movie => movie.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+
+            bool result = _movies.Any(func);
+
+            movie = _movies.FirstOrDefault(func);
+
+            return result;
+        }
+
+        public bool GetMovies(string director, out List<Movie> movies)
+        {
+            Func<Movie, bool> func = movie => movie.Director.Equals(director, StringComparison.OrdinalIgnoreCase);
+
+            bool result = _movies.Any(func);
+
+            movies = _movies.Where(func).ToList();
+
+            return result;
+        }
+
+        public bool UpdateMovie(string title, Genre newGenre)
+        {
+            bool result = GetMovie(title, out Movie? movie);
+
+            if (movie != null)
+            {
+                movie.Genre = newGenre;
+            }
+
+            return result;
+        }
+
+        public bool DeleteMovie(string title)
+        {
+            bool result = GetMovie(title, out Movie? movie);
+
+            if (movie != null)
+            {
+                _movies.Remove(movie);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ hwC#Step/Models/Movie.cs
@@ -0,0 +1,90 @@
+﻿using System.Security;
+
+namespace ConsoleApp_418_FinalProject.Models
+{
+    enum Genre
+    {
+        SciFi,
+        Drama,
+        Horror,
+        Comedy
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Movie
+    {
+        private int _releaseYear;
+        private string? _title;
+        private string? _director;
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+
+                _title = value;
+            }
+        }
+
+        public string Director
+        {
+            get => _director;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+
+                _director = value;
+            }
+        }
+
+        /// <summary>
+        /// Movie's Release Year.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">1990-current year.</exception>
+        public int ReleaseYear
+        {
+            get => _releaseYear;
+            set
+            {
+                if (value < 1990 || value > DateTime.Now.Year)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
+
+                _releaseYear = value;
+            }
+        }
+
+        public Genre Genre { get; set; }
+
+        public Movie(
+            string title,
+            string director,
+            int releaseYear,
+            Genre genre)
+        {
+            this.Title = title;
+            this.Director = director;
+            this.ReleaseYear = releaseYear;
+            this.Genre = genre;
+        }
+
+        public override string ToString()
+        {
+            return $"Title: {this.Title}, " +
+                $"Director: {this.Director}, " +
+                $"ReleaseYear {this.ReleaseYear}, " +
+                $"Genre: {this.Genre}";
+        }
+    }
+}
